21a71cab4de32812e0fc56b47907c08d549a0983
Vu Le <tuongvu@gmail.com>
2018-10-16 11:15:11 +0700

    snapshot.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='unity-tools',
+    version='0.1',
+    scripts=['unity-tools'],
+    author='Vu Le',
+    author_email='tuongvu@gmail.com',
+    description='Unity3D project utilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/finderseyes/unity-tools',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
