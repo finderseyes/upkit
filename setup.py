22b42c7eabada4179bdd51d435d29093422a99a3
Vu Le <tuongvu@gmail.com>
2018-10-18 19:58:59 +0700

    Remove redundant installation script.

--- setup.py
+++ setup.py
@@ -4,22 +4,12 @@ import setuptools
 from sys import platform
 from setuptools.command.install import install
 
-
-class InstallOSXTools(install):
-    def run(self):
-        if platform == 'osx':
-            command = 'brew install hardlink-osx'
-            process = subprocess.Popen(command, shell=True)
-            process.wait()
-            install.run(self)
-
-
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='unity-tools',
-    version='0.1',
+    version='0.1.0',
     scripts=['unity-tools.py'],
     entry_points={
         'console_scripts': [
@@ -28,15 +18,12 @@ setuptools.setup(
     },
     author='Vu Le',
     author_email='tuongvu@gmail.com',
-    description='Unity3D project utilities',
+    description='Project and package manager for Unity3D',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/finderseyes/unity-tools',
     packages=setuptools.find_packages(),
     install_requires=['xmltodict', 'pyyaml', 'jinja2',],
-    cmdclass={
-        'install-osx-tools': InstallOSXTools
-    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
