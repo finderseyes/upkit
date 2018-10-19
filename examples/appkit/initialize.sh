4225d98df9fcd05bb49ec205b9d764f3809fdfd1
Vu Le <tuongvu@gmail.com>
2018-10-19 11:35:11 +0700

    Generate ios and android projects.

--- examples/appkit/initialize.sh
+++ examples/appkit/initialize.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 nuget restore packages.config -PackagesDirectory dependencies
-unity-tools link -c project-config.yaml
+
+unity-tools link -c project-config.yaml -p platform=ios
+unity-tools link -c project-config.yaml -p platform=android
