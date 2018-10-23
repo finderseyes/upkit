2cec2dcef98df0442fe189f6b8e2d5c63e155510
Vu Le <tuongvu@gmail.com>
2018-10-23 20:47:50 +0700

    Change appkit to simple-demo, use new upkit (Nuget resolve)

--- examples/appkit/initialize.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-nuget restore packages.config -PackagesDirectory dependencies
-
-upkit link -c project-config.yaml -w "../../temp/packages" -p platform=ios
-upkit link -c project-config.yaml -w "../../temp/packages" -p platform=android
